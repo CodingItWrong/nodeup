ce947b1f9ce6a6d8e9b20d57bea1d9e1f8f010dd
Josh Justice <josh.justice@accesscfa.com>
2019-09-24 10:54:08 -0400

    Enable async/await

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
   extends: ['eslint:recommended', 'prettier'],
   plugins: ['prettier'],
   env: {
-    es6: true,
     node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
   },
   rules: {
     'prettier/prettier': 'error',
