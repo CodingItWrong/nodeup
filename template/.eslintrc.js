c4d4740003f4466a2902481eb2ea8d2ff1c536d8
Josh Justice <me@codingitwrong.com>
2020-09-07 13:06:20 -0400

    Add Jest ESLint plugin

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: ['jest'],
   env: {
     es6: true,
+    'jest/globals': true,
     node: true,
   },
   parserOptions: {
     ecmaVersion: 2017,
   },
-}
+};
